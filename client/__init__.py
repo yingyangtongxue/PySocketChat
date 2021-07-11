2f208629e3c0928e179cfdcfc7fcd99802d81bf6
Ellyzeul <gabrielaug.240@gmail.com>
2021-07-11 01:12:47 -0300

    feat: creating client module

---
+++ client/__init__.py
@@ -0,0 +1,15 @@
+import socket
+import json
+
+
+def create_server():
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    with open('configs.json') as json_file:
+        configs = json.load(json_file)
+
+    connection.bind((socket.gethostname(), configs["port"]))
+
+    connection.listen(5)
+
+    return connection
