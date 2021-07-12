e74e58084bd15e3a291182860c5501a819977792
Ellyzeul <gabrielaug.240@gmail.com>
2021-07-11 21:15:11 -0300

    docs: documenting functions
    Creating documentation for client.create_client() and server.create_server()

--- client/__init__.py
+++ client/__init__.py
@@ -3,6 +3,8 @@ import json
 
 
 def create_client():
+    """Função para criar instância de um cliente"""
+
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     return connection
--- server/__init__.py
+++ server/__init__.py
@@ -3,6 +3,8 @@ import json
 
 
 def create_server():
+    """Função para criar uma instância de um servidor"""
+
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     with open('server/configs.json') as json_file:
