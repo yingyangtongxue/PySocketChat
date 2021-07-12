38c03cef6bdb3a0e586212cca642c67816422ab2
Ellyzeul <gabrielaug.240@gmail.com>
2021-07-11 21:10:55 -0300

    refactor: splitting run.run_client() and run.run_server() into run_client.run() and run_server.run()
    Doing things this way will decouple client and server, making them independent from each other.

---
+++ run_client.py
@@ -0,0 +1,21 @@
+from client import create_client
+import socket
+import json
+
+
+def run():
+    """Função para instanciar cliente e conectar com servidor"""
+
+    client = create_client()
+
+    with open('server/configs.json') as json_file:
+        configs = json.load(json_file)
+        json_file.close()
+
+    client.connect((socket.gethostname(), configs["port"]))
+
+    while True:
+        message = client.recv(1024)
+        print(message.decode("utf-8"))
+
+run()
---
+++ run_server.py
@@ -0,0 +1,17 @@
+from server import create_server
+from threading import Thread
+import json
+
+
+def run():
+    """Função para instanciar servidor e ficar escutando requisição do cliente"""
+
+    server = create_server()
+
+    while True:
+        connectedsocket, address = server.accept()
+        print("server from {address} established".format(address = address))
+        connectedsocket.send(bytes(json.dumps({"response": "response here!!"}), "utf-8"))
+
+
+run()
