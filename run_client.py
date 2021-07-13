c1b79db3161cbb5a5c1449a18b0d283a7de9a6c5
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2021-07-13 19:47:16 -0300

    Revert "Adding function to enable special characters on Windows"

--- run_client.py
+++ run_client.py
@@ -2,24 +2,13 @@ from threading import Thread
 from client import create_client
 import socket
 import json
-import sys
-import os
 
-def active_windows_special_char():
-    """Função para ativar os caracteres especiais ASCII no Windows. 
-    Não há necessidade no Linux"""
-    if os.name == 'nt':                             
-        from ctypes import windll                   
-        k = windll.kernel32
-        k.SetConsoleMode(k.GetStdHandle(-11), 7)
 
-def listen_messages(client, display):
+def listen_messages(client):
     while True:
-        msg = client.recv(4096).decode("utf-8") # recebe a mensagem do servidor
-        sys.stdout.write('\x1b[0G')             # move o cursor do terminal para o inicio da linha
-        sys.stdout.write('\x1b[0K')             # apaga toda a linha a partir do cursor até o fim
-        print(msg)                              # imprime a mensagem do servidor
-        print(display, end="", flush=True)      # imprime o display para inserção de nova mensagem
+        msg = client.recv(4096)
+        print(msg.decode())
+
 
 def run():
     """Função para instanciar cliente e conectar com servidor"""
@@ -32,27 +21,20 @@ def run():
 
     server_addr = (socket.gethostname(), configs["port"])
     client.connect(server_addr)
+    
 
     try:
-        active_windows_special_char()
         username = input('Your name? ')
-        display = f'{username}> '
 
-        t = Thread(
-            target = listen_messages, 
-            args = (client, display,)
-        )
+        t = Thread(target = listen_messages, args = (client,))
         t.daemon = True
         t.start()
 
-        print("welcome to the chat\n\n")
         while True:
-            to_send = input(display)
-            client.sendto(bytes(f"{display} {to_send}", "utf-8"), (socket.gethostname(), configs["port"]))
+            to_send = input(f'{username}> ')
+            
 
     except KeyboardInterrupt:
-        client.sendto(bytes("\\quit", "utf-8"), (socket.gethostname(), configs["port"]))
-        client.close()
         print('\n\nSee ya..')  
         exit()
     
--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-import threading
-
-def active_threads_test():
-    while True:
-        sleep(8)
-        print("Number of active threads: ",threading.active_count())
