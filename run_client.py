8e99fa884aa2d2f2f4ef5fe71a05d1527fde2592
Ellyzeul <gabrielaug.240@gmail.com>
2021-07-12 23:39:16 -0300

    feat: creating clients communication
    As clients connect to the server, every client will see the messages sent by other clients, like a global chat

--- run_client.py
+++ run_client.py
@@ -2,13 +2,16 @@ from threading import Thread
 from client import create_client
 import socket
 import json
+import sys
 
 
-def listen_messages(client):
+def listen_messages(client, display):
     while True:
-        msg = client.recv(4096)
-        print(msg.decode())
-
+        msg = client.recv(4096).decode("utf-8") # recebe a mensagem do servidor
+        sys.stdout.write('\x1b[0G')             # move o cursor do terminal para o inicio da linha
+        sys.stdout.write('\x1b[0K')             # apaga toda a linha a partir do cursor até o fim
+        print(msg)                              # imprime a mensagem do servidor
+        print(display, end="", flush=True)                # imprime o display para inserção de nova mensagem
 
 def run():
     """Função para instanciar cliente e conectar com servidor"""
@@ -21,20 +24,26 @@ def run():
 
     server_addr = (socket.gethostname(), configs["port"])
     client.connect(server_addr)
-    
 
     try:
         username = input('Your name? ')
+        display = f'{username}> '
 
-        t = Thread(target = listen_messages, args = (client,))
+        t = Thread(
+            target = listen_messages, 
+            args = (client, display,)
+        )
         t.daemon = True
         t.start()
 
+        print("welcome to the chat\n\n")
         while True:
-            to_send = input(f'{username}> ')
-            
+            to_send = input(display)
+            client.sendto(bytes(f"{display} {to_send}", "utf-8"), (socket.gethostname(), configs["port"]))
 
     except KeyboardInterrupt:
+        client.sendto(bytes("\\quit", "utf-8"), (socket.gethostname(), configs["port"]))
+        client.close()
         print('\n\nSee ya..')  
         exit()
     
