12439ac4f3e8167b97a673f166d7ed6f9513d28b
Ellyzeul <gabrielaug.240@gmail.com>
2021-07-13 23:31:51 -0300

    docs: commenting code behaviour
    I know this is a bad practice, but since this project initially is for a university homework of computer networking discipline, and the teacher requires for it, it became indispensable

--- client/__init__.py
+++ client/__init__.py
@@ -3,8 +3,13 @@ import json
 
 
 def create_client():
-    """Função para criar instância de um cliente"""
+    """Função para criar uma instância de um cliente
+    
+    Caso de uso:
+        client = create_client()
+        
+        # ..."""
 
-    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criando socket IPv4 (AF_INET) com protocolo TCP (SOCK_STREAM)
 
     return connection
--- run_client.py
+++ run_client.py
@@ -6,10 +6,10 @@ import sys
 import os
 
 def active_windows_special_char():
-    """Função para ativar os caracteres especiais ASCII no Windows. 
+    """Função para ativar os caracteres de escape ANSI no Windows. 
     Não há necessidade no Linux"""
-    if os.name == 'nt':                             
-        from ctypes import windll                   
+    if os.name == 'nt':
+        from ctypes import windll
         k = windll.kernel32
         k.SetConsoleMode(k.GetStdHandle(-11), 7)
 
@@ -26,7 +26,7 @@ def run():
 
     client = create_client()
 
-    with open('server/configs.json') as json_file:
+    with open('server/configs.json') as json_file: # Abre o JSON com as configurações que serão usadas
         configs = json.load(json_file)
         json_file.close()
 
@@ -34,7 +34,7 @@ def run():
     client.connect(server_addr)
 
     try:
-        active_windows_special_char()
+        active_windows_special_char()           # Faz tratamento de caracteres especiais
         username = input('Your name? ')
         display = f'{username}> '
 
@@ -43,18 +43,19 @@ def run():
             args = (client, display,)
         )
         t.daemon = True
-        t.start()
+        t.start()                               # Inicia uma thread para ficar escutando mensagens que chegam do servidor
 
         print("welcome to the chat\n\n")
         while True:
             to_send = input(display)
-            client.sendto(bytes(f"{display} {to_send}", "utf-8"), (socket.gethostname(), configs["port"]))
+            client.sendto(bytes(f"{display} {to_send}", "utf-8"), (socket.gethostname(), configs["port"])) # Envia a mensagem digitada pelo cliente atual para o servidor
 
-    except KeyboardInterrupt:
-        client.sendto(bytes("\\quit", "utf-8"), (socket.gethostname(), configs["port"]))
+    except KeyboardInterrupt: # Em caso de derrubar o servidor com Ctrl+C
+        client.sendto(bytes("\\quit", "utf-8"), (socket.gethostname(), configs["port"])) # Mandando mensagem ao servidor para desconexão
+        client.shutdown()
         client.close()
         print('\n\nSee ya..')  
         exit()
-    
+
 
 run()
